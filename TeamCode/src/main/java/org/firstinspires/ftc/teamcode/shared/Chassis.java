422dd7f4d6d717a18cb8f7c0c2a3dc3d1eb18c77
Edwin Ryerson <eryerson06@gmail.com>
2021-04-17 08:48:22 -0600

    Good Runs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/Chassis.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 
-
+//Runs Completed
 public class Chassis {
     private DriveWheel frontLeft;
     private DriveWheel frontRight;
