7cdcd0787eb588fb6988f198201393b6521476e7
Edwin Ryerson <eryerson06@gmail.com>
2021-10-07 13:14:51 -0600

    liF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/FreeformAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/FreeformAuto.java
@@ -49,7 +49,7 @@ public class FreeformAuto extends LinearOpMode {
         Chassis chassis = robot.getChassis();
 
         waitForStart();
-        chassis.driveStraight(1, .5);
+        chassis.driveStraight(10, .25);
         //chassis.strafe(4, .5, true);
         //chassis.downLift();
         sleep(1000);
